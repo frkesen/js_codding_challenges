2019c4070aeb7f6f73251b56b1307a534f17a576
Faruk <felixfranko66@gmail.com>
2021-01-09 02:49:07 +0300

    added new solution

--- removeDuplicateWords.js
+++ removeDuplicateWords.js
@@ -1,9 +1,21 @@
 let str = "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta";
 
-function removeDuplicateWords() {
-    const word = [...new Set(str.split(" "))].join(' ');
-    return word;
-}
+// Solution 1
+// function removeDuplicateWords() {
+//     const word = [...new Set(str.split(" "))].join(' ');
+//     return word;
+// }
 
+// Solution 2
+function removeDuplicateWords() {
+    const words = str.split(" ");
+    newWords = [];
+    for (word of words) {
+        if (!newWords.includes(word))
+            newWords.push(word);
+}
+    
+    return newWords.join(" ");
+}
 
 console.log(removeDuplicateWords()) ;
